7b1a69b03f1a23540a729fe6d388e37dd250df6f
Theis-Mathiassen <78611080+Theis-Mathiassen@users.noreply.github.com>
2023-01-29 19:29:25 +0100

    Add project files.

---
+++ TimeRecorder/Shared/Models/TimeRecording.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeRecorder.Shared.Models
+{
+    public class TimeRecording : IEquatable<TimeRecording>
+    {
+        public Int64 ID { get; private set; }
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+        public DateOnly Date { get; set; }
+        public string Description { get; set; }
+
+        public TimeRecording () : this (TimeOnly.FromDateTime(DateTime.Now), TimeOnly.FromDateTime(DateTime.Now), DateOnly.FromDateTime(DateTime.Now), "")
+        {
+        }
+        public TimeRecording(TimeOnly start, TimeOnly end, DateOnly date, string description)
+        {
+            ID = UniqueIdGenerator.GenerateId();
+            Start = start;
+            End = end;
+            Date = date;
+            Description = description;
+        }
+
+        public DateOnly GetDate ()
+        {
+            return Date;
+        }
+
+        public TimeSpan GetDuration ()
+        {
+            return End - Start;
+        }
+
+        public override string ToString()
+        {
+            return "Start: " + Start + ", End: " + End + ", Description: " + Description;
+        }
+
+
+        public bool Equals(TimeRecording? other)
+        {
+            if (other == null) return false;
+            return ID.Equals(other.ID);
+        }
+    }
+}
---
+++ TimeRecorder/Shared/Models/UniqueIdGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeRecorder.Shared.Models
+{
+
+    public class UniqueIdGenerator
+    {
+        private static TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+        private static Random random = new Random();
+        private static int currentId = 0;
+        private static readonly object lockObject = new object();
+
+        public static Int64 GenerateId()
+        {
+            lock (lockObject)
+            {
+                return Int64.Parse("" + t.Seconds + random.Next(512) + ++currentId);
+            }
+        }
+    }
+
+}
